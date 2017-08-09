56b4388e5c103b1f1c603571fea4b05686a47fb1
Katie Fritz <katie@katiemfritz.com>
2017-08-09 14:47:30 -0400

    change port for real; deployment prep

--- pages/about.vue
+++ pages/about.vue
@@ -7,13 +7,13 @@
       <p>Here's what you should know about me.</p>
 
       <h2>I'm a geek.</h2>
-      <p>Geek isn’t a bad word – it means I love to learn. These days, I mostly learn about project management, information architecture, user experience design, content strategy, taxes, and crochet (gotta have hobbies!). Earlier in my career, I’ve geeked nonprofit tech strategy, IT systems, association management, asset building policy, housing policy, and music. I still enjoy learning (and teaching) about all of those things.</p>
+      <p>Geek isn’t a bad word – it means I love to learn. These days, I mostly learn about content strategy, design systems, project management, information architecture, user experience design, and Vue.js. Earlier in my career, I’ve geeked nonprofit tech strategy, IT systems, association management, asset building policy, and housing policy. I still enjoy learning (and teaching) about all of those things.</p>
 
       <h2>I like people.</h2>
-      <p>All of my projects put people at the center. Who visits your website? Why? How do they feel? What mortal humans have to maintain the site, and how can we make it easier? We’ll talk, a lot, and I’ll interview and observe as many people as I can to help make your website better.</p>
+      <p>All of my projects put people at the center. Who visits your website? Why? How do they feel? Who has to maintain the site, and how can we make it easier? We’ll talk, a lot, and I’ll interview and observe as many people as I can to help make your website better.</p>
 
       <h2>I want to make an actual difference.</h2>
-      <p>Like my clients, I want to make the world better, however I can. I’ve spent my whole career working in, volunteering for, and providing services to the nonprofit sector.  Not every nonprofit makes the world better, and most websites don’t really help. These days, that’s my focus. I get what it’s like to have a small tech budget, limited time, but a big mission! Together we can build your capacity so you can do more good.</p>
+      <p>Like my clients, I want to make the world better, however I can. I’ve spent my whole career working in, volunteering for, and providing services to the nonprofit and education sectors. I get what it’s like to have a small tech budget, limited time, but a big mission! Together we can build your capacity so you can do more good.</p>
     </section>
     <o-past-clients/>
     <m-cta :message="cta"/>
