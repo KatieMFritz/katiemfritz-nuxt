03539ee3532a0d9093a826e8abce446f998c4a54
Katie Fritz <katie@katiemfritz.com>
2017-09-13 20:11:45 -0400

    more icon route tweaking

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,6 @@ module.exports = {
     ],
     link: [
       // Favicons
-      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' },
